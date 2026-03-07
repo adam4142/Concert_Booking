bc43bcbd707e73ce188be6184c1b917b99e4bf8e
Adam Philip <adamphilip9@gmail.com>
2026-03-08 00:52:48 +0530

    Added Authentication

--- src/main/java/org/example/concert_booking/SecurityConfig/Security.java
+++ src/main/java/org/example/concert_booking/SecurityConfig/Security.java
@@ -1,21 +1,20 @@
 package org.example.concert_booking.SecurityConfig;
 
+import org.example.concert_booking.Security.ApiAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 //import org.example.concert_booking.Service.CustomUserDetailsService;
-
-
-
-
 
 
 @Configuration
@@ -25,6 +24,9 @@ public class Security {
 
 //    @Autowired
 //    CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    ApiAuthenticationFilter apiAuthenticationFilter;
 
     @Bean
     public static PasswordEncoder passwordEncoder() {
@@ -40,7 +42,7 @@ public class Security {
 
                         // ADMIN endpoints FIRST
                         .requestMatchers("/api/add_concert","/api/editConcert/**","/api/deleteConcert/**")
-                        .hasAuthority("ADMIN")
+                        .hasRole("ADMIN")
 
                         // public endpoints
                         .requestMatchers("/registration","/css/**","/js/**","/api/login","/api/register",
@@ -49,7 +51,7 @@ public class Security {
 
                         .anyRequest().authenticated()
                 )
-                .formLogin(form -> form.disable())
+                .addFilterBefore(apiAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)                .formLogin(form -> form.disable())
                 .logout(form -> form.disable());
 
         return http.build();
